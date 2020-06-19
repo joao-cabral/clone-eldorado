7d1d16079b098e38b434796f9bd4f123c56d6eaa
Joao Cabral <joaocabral6665@gmail.com>
2020-06-18 23:59:59 -0400

    Updates

---
+++ precache-manifest.7fd6ce0644aa83c18e77c0aa5c04e9df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dddb817337527be19d211e64830ee82",
+    "url": "/clone-eldorado/index.html"
+  },
+  {
+    "revision": "141660f39573d2a18140",
+    "url": "/clone-eldorado/static/css/main.9070d776.chunk.css"
+  },
+  {
+    "revision": "53f62cfab9614cac1c76",
+    "url": "/clone-eldorado/static/js/2.82e91a2a.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/clone-eldorado/static/js/2.82e91a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "141660f39573d2a18140",
+    "url": "/clone-eldorado/static/js/main.ca1f6267.chunk.js"
+  },
+  {
+    "revision": "fc2cd2ea94bc4e7c161c",
+    "url": "/clone-eldorado/static/js/runtime-main.27bb0b31.js"
+  }
+]);
